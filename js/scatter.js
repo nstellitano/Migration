e72c1172a0eeab0d9ce10e3d23b82d8374f21421
adhikarisamik <adhikari.samik@gmail.com>
2015-04-16 13:19:33 -0400

    this is a scatter plot push test

--- js/scatter.js
+++ js/scatter.js
@@ -9,7 +9,6 @@
 ScatterVis = function(_parentElement, _data, _metaData){
     this.parentElement = _parentElement;
     this.data = _data;
-    this.metaData = _metaData;
     this.displayData = [];
 
 
